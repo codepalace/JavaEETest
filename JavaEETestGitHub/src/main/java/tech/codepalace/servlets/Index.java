309f2ba44d8bb87531cfdb627a3d5884568db617
Antonio Estevez Gonzalez <eclipsetestteam@gmail.com>
2021-06-02 10:55:34 +0200

    Agregando un mensaje que se desplega en la consola de prueba

--- JavaEETestGitHub/src/main/java/tech/codepalace/servlets/Index.java
+++ JavaEETestGitHub/src/main/java/tech/codepalace/servlets/Index.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.JOptionPane;
 
 @WebServlet("/Index")
 public class Index extends HttpServlet {
@@ -20,6 +21,11 @@ public class Index extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		this.getServletContext().getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(request, response);
+
+		
+		String message ="Mensaje de prueba desde la Servlet a la consola";
+		
+		System.out.println(message);
 	}
 
 	
